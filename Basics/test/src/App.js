c3c96e8caa3d7d69512dabbd33e09a0ea09e070a
yoless2 <yoless2@gmail.com>
2022-01-05 21:05:30 -0500

    first commit

---
+++ Basics/test/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edith <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://i.gifer.com/2xtR.gif"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Loki Pedos Loc05
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
